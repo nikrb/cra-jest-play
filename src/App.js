1d03f578d5765436385c0ebc7db24d6ea6ed2323
nikrb <nik_rb@yahoo.com>
2018-05-03 11:21:15 +0100

    update app to fetch multiple data

--- src/App.js
+++ src/App.js
@@ -3,20 +3,43 @@ import React, { Component } from 'react';
 class App extends Component {
   state = {
     topics: [],
+    subTopics: [],
   };
   componentDidMount() {
-    this.loadTopics().then(topics => this.setState({topics}));
+    this.loadData().then(results => {
+      console.log('load data:', results);
+      const [topics, subTopics, ...rest] = results;
+      this.setState({ topics, subTopics });
+    });
   }
-  async loadTopics() {
-    fetch('/api/v1/topics')
-      .then(res => res.json());
+  loadData() {
+    const promises = [];
+    let p;
+    p = new Promise(resolve => {
+      return fetch('/api/v1/topics')
+        .then(res => {
+          resolve(res.json());
+        });
+      });
+    promises.push(p);
+    p = new Promise(resolve => {
+      return fetch('/api/v1/subtopics')
+          .then(res => resolve(res.json()));
+      }
+    );
+    promises.push(p);
+    return Promise.all(promises);
   }
   render() {
-    const { topics } = this.state;
-    const lis = topics.map(t => <li key={t._id}>{t.name}</li>)
+    const { topics, subTopics } = this.state;
+    const lis = topics.map(t => <li key={t._id}>{t.name}</li>);
+    const sublist = subTopics.map(s => <li key={s._id}>{s.name}</li>);
     return (
       <div>
+        <h2>Topics</h2>
         <ul>{lis}</ul>
+        <h2>SubTopics</h2>
+        <ul>{sublist}</ul>
       </div>
     );
   }
