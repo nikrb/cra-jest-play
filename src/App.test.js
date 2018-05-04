a3545872a896a110b6a9abd6d49cdd824428e404
nikrb <nik_rb@yahoo.com>
2018-05-04 01:41:06 +0100

    add more tests

--- src/App.test.js
+++ src/App.test.js
@@ -9,12 +9,15 @@ import App from './App';
 const topics = [
   { _id: '1', name: 'Voyage', order: 1 },
 ];
+const subTopics = [
+  { _id: '2', parent: '1', name: 'About Voyages', order: 2 },
+];
 
 it('should display topic list', () => {
   const topicSpy = jest
-    .spyOn(App.prototype, 'loadTopics')
+    .spyOn(App.prototype, 'loadData')
     .mockImplementation(() => {
-      return new Promise(resolve => resolve(topics))
+      return new Promise(resolve => resolve([topics, subTopics]))
     });
 
   const comp = shallow(<App />);
@@ -23,7 +26,11 @@ it('should display topic list', () => {
 
   return Promise.resolve().then(async () => {
     comp.update();
-    // expect(comp.instance().state).toEqual({ topics });
-    expect(comp.find('li').length).toBe(1);
+    const topicList = comp.find('ul').at(0);
+    const tele = topicList.find('li').at(0).childAt(0);
+    expect(tele.text()).toBe('Voyage');
+    const subList = comp.find('ul').at(1);
+    const sele = subList.find('li').at(0).childAt(0);
+    expect(sele.text()).toBe('About Voyages');
   });
 });
